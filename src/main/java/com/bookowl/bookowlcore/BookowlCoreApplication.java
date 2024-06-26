42b422b412f3288fceabf932b18bea923f9cb337
skysrd <skysrd@gmail.com>
2024-06-26 17:19:39 +0900

    Initial Commit

---
+++ src/main/java/com/bookowl/bookowlcore/BookowlCoreApplication.java
@@ -0,0 +1,13 @@
+package com.bookowl.bookowlcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookowlCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookowlCoreApplication.class, args);
+    }
+
+}
